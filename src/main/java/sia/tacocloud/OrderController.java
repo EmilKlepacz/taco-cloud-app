4365f8b0e18357fa36ff66e0fc192a095af2f906
Emil Klepacz <klepaczemil@gmail.com>
2024-09-26 14:20:04 +0200

    order form and OrderController for it added.^

---
+++ src/main/java/sia/tacocloud/OrderController.java
@@ -0,0 +1,38 @@
+package sia.tacocloud;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+@SessionAttributes("tacoOrder")
+public class OrderController {
+
+    //@ModelAttribute("tacoOrder"):
+    // This method ensures that a TacoOrder object is added
+    // to the model before rendering the form.
+    // If tacoOrder already exists in the session
+    // (because of @SessionAttributes), it will be used,
+    // otherwise,
+    // a new TacoOrder is created and added to the session and model.
+    @ModelAttribute(name = "tacoOrder")
+    public TacoOrder tacoOrder() {
+        return new TacoOrder();  // Initialize TacoOrder and add it to the model
+    }
+
+    @PostMapping
+    public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+        log.info("Order submitted: {}", order);
+        sessionStatus.setComplete();
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/current")
+    public String orderForm() {
+        return "orderForm";
+    }
+}
